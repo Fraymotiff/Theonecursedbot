633df57a7b5238c0ca00d23d0f13722f4a1e05bf
Fraymotiff <68887087+Fraymotiff@users.noreply.github.com>
2020-07-29 23:11:00 -0300

    pleasework

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require ('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('Russian Roulette bot is now online!', () => {
+     //Code for the bot to do when starting up.
+     })
+
+const PREFIX ='!'
+
+
+
+client.once('ready', () => {
+     //Code for the bot to do when starting up.
+     })
+     console.log('Russian Roulette is now online!')
+   
+     client.on('message', (message) => {
+     const PREFIX = '!'
+     
+
+     if (message.content.startsWith (PREFIX + "cursed")) {
+         let imageNumber = Math.floor(Math.random()* 100) +1
+             message.channel.send ("Let's Give it everything we got! It's Punishment time!",  {files: ["./cursed/" + imageNumber + ".png"]} )
+             return; 
+          
+              
+             
+             
+     }
+     });
+    
+    
+
+
+
+    client.login(process.env.token);
+
